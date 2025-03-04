01508ac5fd6aa11764fa4d3ab170f99aa9da6c63
mandoo1229 <skaalsdn35@gmail.com>
2025-03-04 18:33:38 +0900

    Test : 2025.03.04 빠른 A+B

--- src/study/march04.java
+++ src/study/march04.java
@@ -1,16 +1,26 @@
 package study;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class march04 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
-        sc.close();
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-        System.out.println(a + b + c);
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+
     }
 }
