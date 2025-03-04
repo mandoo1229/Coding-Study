57ee107f100e15e269c3510fb78d80d4ad9dc1e2
mandoo1229 <skaalsdn35@gmail.com>
2025-03-04 18:27:59 +0900

    Test : 2025.03.04 꼬마 정민

---
+++ src/study/march04.java
@@ -0,0 +1,16 @@
+package study;
+
+import java.util.Scanner;
+
+public class march04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        sc.close();
+
+
+        System.out.println(a + b + c);
+    }
+}
