3f413495f3ee5505863f60dd415c1b8d3f16b2b4
mandoo <skaalsdn35@gmail.com>
2025-03-09 07:23:14 +0900

    Test : 단어의 개수 3월 10일

---
+++ src/study/march10.java
@@ -0,0 +1,17 @@
+package study;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class march10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.nextLine();
+        sc.close();
+
+        StringTokenizer st = new StringTokenizer(S, " ");
+
+        System.out.println(st.countTokens());
+    }
+}
