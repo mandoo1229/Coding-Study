a2ad1cca51881690662981045a29ee3b8fb04b98
mandoo1229 <skaalsdn35@gmail.com>
2025-03-21 13:02:59 +0900

    Test : 2025.03.20 피보나치수

--- src/study/march21.java
+++ src/study/march21.java
@@ -5,19 +5,5 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class march21 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] number = new int[N + 1];
-
-        number[0] = 0;
-        number[1] = 1;
-
-        for (int i = 2; i <= N; i++) {
-            number[i] = number[i - 1] + number[i - 2];
-        }
-        System.out.println(number[N]);
-
-    }
 }
