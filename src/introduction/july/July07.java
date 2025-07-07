9d4e480646aa21aebe574c958908e178a6dedc64
mandoo1229 <skaalsdn35@gmail.com>
2025-07-07 17:10:03 +0900

    Study : 달팽이는 올라가고 싶다

---
+++ src/introduction/july/July07.java
@@ -0,0 +1,21 @@
+package introduction.july;
+
+import java.util.Scanner;
+
+public class July07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int up = sc.nextInt();
+        int down = sc.nextInt();
+        int length = sc.nextInt();
+
+        int day = (length - down) / (up - down);
+        if ((length - down) % (up - down) != 0) {
+            day++;
+        }
+
+        System.out.println(day);
+
+    }
+}
